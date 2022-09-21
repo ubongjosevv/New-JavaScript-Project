9170d0fb8a31c00e86cadfdb3cce2be15ca3bbea
ubongjosevv <ubeejoseph@gmail.com>
2022-09-21 15:41:42 +0100

    updated

--- script.js
+++ script.js
@@ -2,11 +2,9 @@
 function hide() {
   const hidden = document.querySelector(".navbar");
   hidden.style.display = 'none';
-  hidden.style.animation = 'ease-out';
 }
 
 function doThis() {
   const showUp = document.querySelector(".navbar");
   showUp.style.display = '';
-  showUp.style.animation = 'ease-in';
 }
